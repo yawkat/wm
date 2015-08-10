676ad9398239cbd68055b48dbf8a7b466c684bdf
Jonas Konrad <me@yawk.at>
2015-08-10 16:01:45 +0200

    Better logging for process output

--- src/main/java/at/yawk/wm/tac/launcher/ApplicationRunner.java
+++ src/main/java/at/yawk/wm/tac/launcher/ApplicationRunner.java
@@ -1,11 +1,15 @@
 package at.yawk.wm.tac.launcher;
 
 import at.yawk.yarn.Component;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -14,9 +18,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class ApplicationRunner {
+    private final AtomicInteger processCounter = new AtomicInteger(0);
+
     public void run(Command command) {
         try {
-            log.info("Executing {}", command);
+            int id = processCounter.incrementAndGet();
+            log.info("[{}] Executing {}", id, command);
 
             List<String> line;
             if (command.isJail()) {
@@ -28,15 +35,34 @@ public class ApplicationRunner {
                 line = Arrays.asList(command.getCommand());
             }
 
-            log.debug("Final command line: {}", line);
+            log.debug("[{}] Final command line: {}", id, line);
 
-            new ProcessBuilder()
+            Process process = new ProcessBuilder()
                     .command(line)
-                    .redirectError(ProcessBuilder.Redirect.INHERIT)
-                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .redirectError(ProcessBuilder.Redirect.PIPE)
+                    .redirectOutput(ProcessBuilder.Redirect.PIPE)
                     .start();
+
+            logStream(id, "out", process.getInputStream());
+            logStream(id, "err", process.getErrorStream());
         } catch (IOException e) {
             log.warn("Failed to run command {}", command, e);
         }
+    }
+
+    private void logStream(int processId, String streamName, InputStream stream) {
+        Thread thread = new Thread(() -> {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    log.debug("[{}:{}] {}", processId, streamName, line);
+                }
+            } catch (IOException e) {
+                log.warn("Failed to log [{}:{}]", processId, streamName);
+            }
+        });
+        thread.setName("Process Logger [" + processId + ":" + streamName + "]");
+        thread.setDaemon(true);
+        thread.start();
     }
 }
