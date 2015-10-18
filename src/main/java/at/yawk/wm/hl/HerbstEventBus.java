155362f62434ff5857518f24767849758e4d5b45
Jonas Konrad <me@yawk.at>
2015-10-18 19:50:45 +0200

    Catch failures in the herbst event bus

--- src/main/java/at/yawk/wm/hl/HerbstEventBus.java
+++ src/main/java/at/yawk/wm/hl/HerbstEventBus.java
@@ -4,11 +4,14 @@ import at.yawk.yarn.AcceptMethods;
 import at.yawk.yarn.AnnotatedWith;
 import at.yawk.yarn.Component;
 import java.util.List;
+import java.util.function.BiConsumer;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author yawkat
  */
+@Slf4j
 @Component
 public class HerbstEventBus {
     @AnnotatedWith(Subscribe.class)
@@ -27,20 +30,24 @@ public class HerbstEventBus {
     List<TitleEvent.Handler> titleEventHandlers;
 
     void post(ShutdownEvent shutdownEvent) {
-        for (ShutdownEvent.Handler handler : shutdownEventHandlers) {
-            handler.handle(shutdownEvent);
-        }
+        forEach(shutdownEventHandlers, shutdownEvent, ShutdownEvent.Handler::handle);
     }
 
     void post(TagEvent tagEvent) {
-        for (TagEvent.Handler handler : tagEventHandlers) {
-            handler.handle(tagEvent);
-        }
+        forEach(tagEventHandlers, tagEvent, TagEvent.Handler::handle);
     }
 
     void post(TitleEvent titleEvent) {
-        for (TitleEvent.Handler handler : titleEventHandlers) {
-            handler.handle(titleEvent);
+        forEach(titleEventHandlers, titleEvent, TitleEvent.Handler::handle);
+    }
+
+    private static <T, P> void forEach(Iterable<T> iterable, P parameter, BiConsumer<T, P> actions) {
+        for (T t : iterable) {
+            try {
+                actions.accept(t, parameter);
+            } catch (Throwable e) {
+                log.error("Failed to pass event", e);
+            }
         }
     }
 }
