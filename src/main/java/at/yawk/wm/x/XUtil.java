636875e44707d9d1de4f75fc16d2187b93e6d1d4
yawkat <me@yawk.at>
2017-04-11 12:00:32 +0200

    Slice images into smaller parts before sending them to X, if necessary

--- src/main/java/at/yawk/wm/x/XUtil.java
+++ src/main/java/at/yawk/wm/x/XUtil.java
@@ -1,6 +1,7 @@
 package at.yawk.wm.x;
 
 import at.yawk.wm.x.image.LocalImage;
+import java.nio.ByteBuffer;
 import org.freedesktop.xcb.LibXcb;
 import org.freedesktop.xcb.SWIGTYPE_p_xcb_connection_t;
 import org.freedesktop.xcb.xcb_image_format_t;
@@ -24,20 +25,36 @@ public class XUtil {
      */
     public static void putImage(SWIGTYPE_p_xcb_connection_t connection, int drawable, int graphics, short depth,
                                 int x, int y, int width, int height, LocalImage image) {
+        if (width == 0 || height == 0) { return; }
+
         ZFormatImage zImage = image.as(ZFormatImage.TYPE);
-        LibXcb.xcb_put_image(
-                connection,
-                (short) xcb_image_format_t.XCB_IMAGE_FORMAT_Z_PIXMAP,
-                drawable,
-                graphics,
-                width,
-                height,
-                (short) x,
-                (short) y,
-                (short) 0,
-                depth,
-                zImage.getBuffer().capacity(),
-                zImage.getBuffer()
-        );
+
+        int rowsPerCall = 0x300000 / width;
+        int imageY = 0;
+        while (height > 0) {
+            int rowsToSend = Math.min(rowsPerCall, height);
+            ByteBuffer duplicate = zImage.getBuffer().duplicate();
+            duplicate.position(imageY * zImage.getWidth() * 4);
+            duplicate.limit(duplicate.position() + zImage.getWidth() * rowsToSend * 4);
+            ByteBuffer slice = duplicate.slice();
+
+            LibXcb.xcb_put_image(
+                    connection,
+                    (short) xcb_image_format_t.XCB_IMAGE_FORMAT_Z_PIXMAP,
+                    drawable,
+                    graphics,
+                    width,
+                    rowsToSend,
+                    (short) x,
+                    (short) y,
+                    (short) 0,
+                    depth,
+                    slice.capacity(),
+                    slice
+            );
+            y += rowsToSend;
+            imageY += rowsToSend;
+            height -= rowsToSend;
+        }
     }
 }
