c7764810540d0fc78152eaaee1ca505c132bdddc
Jonas Konrad <me@yawk.at>
2021-04-09 09:59:13 +0200

    fix genpw16

--- src/main/java/at/yawk/wm/tac/launcher/LauncherConfig.kt
+++ src/main/java/at/yawk/wm/tac/launcher/LauncherConfig.kt
@@ -7,7 +7,7 @@ object LauncherConfig {
         "quasselclient" to Command(listOf("/home/yawkat/dev/mirrors/quassel/build/quasselclient")),
         "subl3" to Command(listOf("subl3")),
         "genpw16" to Command(listOf("bash", "-c", "< /dev/urandom tr -dc a-z0-9 | head -c\${1:-16} | xclip -selection c")),
-        "genpw16" to Command(listOf("bash", "-c", "< /dev/urandom tr -dc a-z0-9 | head -c\${1:-32} | xclip -selection c")),
+        "genpw32" to Command(listOf("bash", "-c", "< /dev/urandom tr -dc a-z0-9 | head -c\${1:-32} | xclip -selection c")),
         "spotify" to Command(listOf("spotify", "--force-device-scale-factor=1.0000001")),
     )
     val shutdownCommand = listOf("systemctl", "poweroff")
